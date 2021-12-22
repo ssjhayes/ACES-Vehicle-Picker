9ab7401870b447af2a0b821118a2b7472e7639e1
Korey Hayes <soccerhayes@gmail.com>
2021-12-22 13:48:13 -0700

    initial models

--- VehiclePicker/vehicle_api/models.py
+++ VehiclePicker/vehicle_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+# Set up the models for dependent dropdown for vehicle selection
+class Make(models.Model):
+	name = models.CharField(max_length=50)
+
+class Model(models.Model):
+	name = models.CharField(max_length=50)
+	make = models.ForeignKey(Make, on_delete=models.CASCADE)
+
+class Submodel(models.Model):
+	name = models.CharField(max_length=50)
+	model = models.ForeignKey(Model, on_delete=models.CASCADE)
+
+class Year(models.Model):
+	year = models.IntegerField()
+	submodel = models.ManyToManyField(Submodel)
+
+# Model for housing complete vehicle information
+class Vehicle(models.Model):
+	make = models.ForeignKey(Make, on_delete=models.CASCADE)
+	model = models.ForeignKey(Model, on_delete=models.CASCADE)
+	submodel = models.ForeignKey(Submodel, on_delete=models.CASCADE)
+	year = models.ForeignKey(Year, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return (self.year.year + ' ' + self.make.name + ' ' + self.model.name + ' ' + self.submodel.name)
